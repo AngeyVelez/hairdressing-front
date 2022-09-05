8feceba86e03a0b96bd4be5487a47b4c9e0214b3
edriball121 <edriball@gmail.com>
2022-09-05 16:15:22 -0500

    Se grega validación nativa a los campos del login

--- src/app/pages/public/auth/sign-in/sign-in.component.ts
+++ src/app/pages/public/auth/sign-in/sign-in.component.ts
@@ -7,6 +7,7 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { User } from '../../../private/user/_services/user.service';
 
 @Component({
+  selector: 'app-login',
   templateUrl: './sign-in.component.html',
   styleUrls: ['./sign-in.component.scss'],
 })
@@ -19,13 +20,16 @@ export class SignInComponent implements OnInit {
     private router: Router,
     private activatedRoute: ActivatedRoute,
     private authService: AuthService,
-    //private _formBuilder: FormBuilder,
-    //public dialogRef: MatDialogRef<SignInComponent>,
-    //@Inject(MAT_DIALOG_DATA) public data: User
+    private _formBuilder: FormBuilder,
   ) {
     this.returnUrl =
       this.activatedRoute.snapshot.queryParamMap.get('returnUrl') ||
       `/${Path.Dashboard}`;
+
+    this.newLoginFormGroup = this._formBuilder.group({
+      email: [''],
+      password: [''],
+    });
   }
 
   onClickSignIn(): void {
@@ -34,26 +38,5 @@ export class SignInComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    /*this.newLoginFormGroup = this._formBuilder.group({
-      email: [this.data?.email ?? '', [Validators.compose([Validators.required, Validators.pattern('[ÑA-Zña-z0-9._%+-]{3,}@[a-zA-Z]{3,}([.]{1}[a-zA-Z]{2,}|[.]{1}[a-zA-Z]{2,}[.]{1}[a-zA-Z]{2,})'), Validators.minLength(4), Validators.maxLength(30)])]],
-      password: [this.data?.password ?? '', [Validators.compose([Validators.required, Validators.pattern(''), Validators.minLength(6), Validators.maxLength(15)])]],
-    });
-    this.getErrorMessage()*/
   }
-  /*getErrorMessage() {
-    this.validations = {
-      'email': [
-        { type: 'required', message: 'El correo electrónico es requerido' },
-        { type: 'pattern', message: 'Se debe ingresar un correo electrónico válido' },
-        { type: 'minlength', message: 'Mínimo 4 caracteres' },
-        { type: 'maxlength', message: 'Máximo 30 caracteres' },
-      ],
-      'password': [
-        { type: 'required', message: 'La contraseña es requerida' },
-        { type: 'pattern', message: 'Se debe ingresar una contraseña valida' },
-        { type: 'minlength', message: 'Mínimo 6 caracteres' },
-        { type: 'maxlength', message: 'Máximo 15 caracteres' },
-      ],
-    };
-  }*/
 }
